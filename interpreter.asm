92af25295f6385707d7f3ed0640c96cd37cc60e6
Michael Bennett <michaelbennett.dev@gmail.com>
2019-11-12 20:12:29 -0800

    Work on toggling a bool

--- interpreter.asm
+++ interpreter.asm
@@ -20,6 +20,16 @@ section .data
     it_exit_s: db "exit"
     it_exit_sl: equ $-it_exit_s
 
+    IT_dbenabled_s: db "Enabling debug mode.", 0x0a
+    IT_dbenabled_sl: equ $-IT_dbenabled_s
+    IT_dbdisabled_s: db "Disabling debug mode.", 0x0a
+    IT_dbdisabled_sl: equ $-IT_dbdisabled_s
+
+    IT_log_leadin: db "[Interpreter] "
+    IT_log_leadin_len: equ $-IT_log_leadin
+
+    is_db: db 1
+
 section .text
     ; Declare our entry point
     ; Note: This nasm, ld, and OS combination seems to 
@@ -42,7 +52,7 @@ _main_loop:
     ; Save RAX
     mov QWORD [rbp - 8], rax
 
-    ; Test if it is the same as our test var 
+    ; Enable debug mode?
     mov rdi, QWORD [rbp - 8]
     mov rax, input_var
     mov rsi, it_db_s
@@ -50,7 +60,7 @@ _main_loop:
     call streq
 
     cmp rax, 1
-    je _main_loop 
+    je _main_toggle_debug 
 
     ; Test if it is the same as our test var 
     mov rdi, QWORD [rbp - 8]
@@ -62,6 +72,22 @@ _main_loop:
     cmp rax, 1
     je _main_end 
 
+_main_toggle_debug:
+    cmp 1, [is_db]
+    jne _main_enable_debug
+_main_disable_debug:
+    mov rsi, IT_dbdisabled_s
+    mov rdx, IT_dbdisabled_sl
+    call interpreter_log
+    mov is_db, 0
+    jmp _main_loop
+_main_enable_debug:
+    mov rsi, IT_dbenabled_s
+    mov rdx, IT_dbenabled_sl
+    call interpreter_log
+    mov is_db, 1
+    jmp _main_loop
+
 _main_end:
     mov rsp, rbp
     pop rbp
@@ -69,3 +95,27 @@ _main_end:
     mov rax, 0x2000001 ; Exit syscall
     mov rdi, 0 ; argument 1: exit value, 0 for success
     syscall
+
+interpreter_log:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 18h
+
+    mov QWORD [rbp - 8], rsi
+    mov QWORD [rbp - 16], rdx
+
+    mov rax, 0x2000004 ; SYS_WRITE
+    mov rdi, 1 ; STDOUT
+    mov rsi, IT_log_leadin ; "[Interpreter] "
+    mov rdx, IT_log_leadin_len ; len(IT_log_leadin)
+    syscall ; syscall
+
+    mov rax, 0x2000004 ; SYS_WRITE
+    mov rdi, 1 ; STDOUT
+    mov rsi, QWORD [rbp - 8] ; 
+    mov rdx, QWORD [rbp - 16] ;
+    syscall ; syscall
+
+    mov rsp, rbp
+    pop rbp
+    ret
